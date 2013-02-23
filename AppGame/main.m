10a2b26e0b0eb5499a616ba377991a5cc6d42046
计 炜 <solidji@jimatoMacBook-Air.local>
2013-02-23 16:04:25 +0800

    Initial Commit

---
+++ AppGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppGame
+//
+//  Created by 计 炜 on 13-2-23.
+//  Copyright (c) 2013年 计 炜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
